85b8c7f14a4f5adda2c1ccacf960ef199353fd67
Romini's coding <74459925+dalping@users.noreply.github.com>
2021-10-21 20:26:49 +0900

    Update 숫자 문자열과 영단어.py

--- 숫자 문자열과 영단어.py
+++ 숫자 문자열과 영단어.py
@@ -1,7 +1,6 @@
 def solution(s):
     answer = ''
     words = ['zero','one','two','three','four','five','six','seven','eight','nine']
-    numbers = ['0','1','2','3','4','5','6','7','8','9']
     tmp = ''
     
     for v in s:
